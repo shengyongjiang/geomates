59be49c70b5bda9be1dc429329f33701ffe2d139
Shawn Jiang <shawn@salesfloor.net>
2025-03-23 16:48:55 -0400

    add more agents

--- agent2.lisp
+++ agent2.lisp
@@ -1,10 +1,8 @@
 (load "actr7.y/load-act-r.lisp")
 (load "geomates/act-r-experiment.lisp")
 
-;; (load-act-r-model "code/DeepSeekAgent.lisp")
+(load-act-r-model "code/model-deepseek-agent2.lisp")
 
-(load "code/navigation-functions.lisp")
-(load-act-r-model "code/model-deepseek-agent1.lisp")
 
 (run-environment)  ;; must enable this to run two agents in the same time machine
 (geomates-experiment)
